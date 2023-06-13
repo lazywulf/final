04c2fed6e62950e48ad776b3f23e5e7b75277afb
lct1452 <136250430+lct1452@users.noreply.github.com>
2023-06-14 00:06:07 +0800

    Add files via upload

--- compare_stats/func/plot.js
+++ compare_stats/func/plot.js
@@ -1,72 +1,3 @@
-export function setupCanvas(barChartData, chartContainerName, title = "", xLabel = "", color = "dodgerblue") {
-    const svg_width = 700;
-    const svg_height = 300;
-    const chart_margin = {top:80,right:40,bottom:40,left:80};
-    const chart_width = svg_width - (chart_margin.left + chart_margin.right);
-    const chart_height = svg_height - (chart_margin.top + chart_margin.bottom);
-    
-    const this_svg = d3.select(`.${chartContainerName}`).append("svg")
-    .attr("width", svg_width).attr("height", svg_height)
-    .append("g")
-    .attr('transform', `translate(${chart_margin.left}, ${chart_margin.top})`);
-    
-    const xExtent = d3.extent(barChartData, d=>d.val);
-    const xMax = d3.max(barChartData, d=>d.val);
-    const xScale_v3 = d3.scaleLinear([0, xMax * 1.1], [0, chart_width]);
-    
-    const yScale = d3.scaleBand().domain(barChartData.map(d=>d.danceability))
-    .rangeRound([1, chart_height])
-    .paddingInner(0.05);
-    
-    const bars = this_svg.selectAll('.bar')
-    .data(barChartData)
-    .enter()
-    .append('rect')
-    .attr('class','bar')
-    .attr('x',0)
-    .attr('y',d => yScale(d.danceability))
-    .attr('width',d => xScale_v3(d.val))
-    .attr('height',yScale.bandwidth())
-    .style('fill', color);
-
-    const header = this_svg.append('g').attr('class','bar-header')
-                .attr('transform',`translate(0,${-chart_margin.top/ 2})`)
-                .append('text');
-    header.append('tspan').text(`${title}`);
-
-
-    const xAxis = d3.axisTop(xScale_v3)
-                    .tickSizeInner(-chart_height)
-                    .tickSizeOuter(0); // the boarder line (boarderless)
-    const xAxisDraw = this_svg.append('g')
-                            .attr('class','x axis')
-                            .call(xAxis);
-    const yAxis = d3.axisLeft(yScale).tickSize(0);
-    const yAxisDraw = this_svg.append('g')
-                            .attr('class','y axis')
-                            .call(yAxis);
-    yAxisDraw.selectAll('text').attr('dx','-0.6em');
-
-    this_svg.append("text")
-        .attr("x", chart_width / 2)
-        .attr("y", -chart_margin.top / 2 + 25)
-        .style("text-anchor", "middle")
-        .text(`${xLabel}`);
-
-    this_svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - chart_margin.left + 30)
-        .attr("x",0 - (chart_height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Danceability (* 0.1)");
-}
-
-
-/////
-
-
-
 export function setupCanvas_teach(barChartData, moviesClean) {
     let metric = 'energy'; // let
 
@@ -90,10 +21,21 @@ export function setupCanvas_teach(barChartData, moviesClean) {
         const defaultDelay = 1000;
         const transitionDelay = d3.transition().duration(defaultDelay);
 
+        const color = {
+            "energy": 'orangered',
+            "loudness":'gold',
+            "valence":'pink',
+            "tempo":'blue'
+        };
+
         xAxisDraw.transition(transitionDelay).call(xAxis.scale(xScale_v3));
         yAxisDraw.transition(transitionDelay).call(yAxis.scale(yScale));
 
-        header.select('tspan').text(`Top 15 ${metric} movies ${metric === 'popularity'? '': 'in USD'}`);
+        header.select('tspan').text(`Realtion Between Danceability and ${metric}`)
+        .attr('x',60).attr('y',0)
+        .style('font-size','1.3em').style('fill',color[metric]);
+
+        
 
         bars.selectAll('.bar').data(data, d=>d.danceability).join(
             enter => {
@@ -101,14 +43,21 @@ export function setupCanvas_teach(barChartData, moviesClean) {
                     .attr('x',0).attr('y',d => yScale(d.danceability))
                     .attr('height',yScale.bandwidth())
                     .style('fill','lightcyan').transition(transitionDelay)
-                    .delay((d,i)=>i*20).attr('width',d=>xScale_v3(d[metric])).style('fill', 'dodgerblue')},
+                    .delay((d,i)=>i*20).attr('width',d=>xScale_v3(d[metric]))
+                    .style('fill', color[metric])
+
+                },                    
             update => {
                 update.transition(transitionDelay)
                    .delay((d,i)=>i*20).attr('y', d => yScale(d.danceability))
-                    .attr('width',d=>xScale_v3(d[metric]))}, 
+                    .attr('width',d=>xScale_v3(d[metric]))
+                    .style('fill', color[metric])
+                }, 
             exit => {
                 exit.transition()
-                .duration(defaultDelay/2).style('fill-opacity',0).remove()});
+                .duration(defaultDelay/2).style('fill-opacity',0).remove()
+            }
+        );
 
         d3.selectAll('.bar')
             .on("mouseover", mouseover)
@@ -121,7 +70,7 @@ export function setupCanvas_teach(barChartData, moviesClean) {
     console.log(barChartData);
     const svg_width = 700;
     const svg_height = 500;
-    const chart_margin = {top: 80, right: 80, bottom: 40, left: 250};
+    const chart_margin = {top: 80, right: 80, bottom: 40, left: 100};
     const chart_width = svg_width - (chart_margin.left + chart_margin.right);
     const chart_height = svg_height - (chart_margin.top + chart_margin.bottom);
 
@@ -129,6 +78,15 @@ export function setupCanvas_teach(barChartData, moviesClean) {
                         .attr("width", svg_width).attr("height", svg_height)
                         .append("g")
                         .attr('transform', `translate(${chart_margin.left},${chart_margin.top})`);
+    
+    this_svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - chart_margin.left + 50)
+        .attr("x",0 - (chart_height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Danceability (* 0.1)");
+
 
     const xExtent = d3.extent(barChartData, d => d[metric]);
     let xMax = d3.max(barChartData, d => d[metric]);
@@ -143,8 +101,8 @@ export function setupCanvas_teach(barChartData, moviesClean) {
     let header = this_svg.append('g').attr('class','bar-header')
                 .attr('transform',`translate(0,${-chart_margin.top/2})`)
                 .append('text');
-    header.append('tspan').text('Top 15 xxx movies');
-    header.append('tspan').text('Years:2000-2009')
+    header.append('tspan').text('Relation between Danceability and XXX');
+    header.append('tspan').text('')
                 .attr('x',0).attr('y',20).style('font-size','0.8em').style('fill','#555');
 
 
@@ -156,6 +114,7 @@ export function setupCanvas_teach(barChartData, moviesClean) {
     let yAxis = d3.axisLeft(yScale).tickSize(0);
     let yAxisDraw = this_svg.append('g').attr('class','y axis');
     yAxisDraw.selectAll('text').attr('dx','-0.6em');
+  
 
     update(barChartData);
     
@@ -171,3 +130,72 @@ export function setupCanvas_teach(barChartData, moviesClean) {
     function mouseout(e) {
     }
 }
+
+
+//////
+
+
+// export function setupCanvas(barChartData, chartContainerName, title = "", xLabel = "", color = "dodgerblue") {
+//     const svg_width = 700;
+//     const svg_height = 300;
+//     const chart_margin = {top:80,right:40,bottom:40,left:80};
+//     const chart_width = svg_width - (chart_margin.left + chart_margin.right);
+//     const chart_height = svg_height - (chart_margin.top + chart_margin.bottom);
+    
+//     const this_svg = d3.select(`.${chartContainerName}`).append("svg")
+//     .attr("width", svg_width).attr("height", svg_height)
+//     .append("g")
+//     .attr('transform', `translate(${chart_margin.left}, ${chart_margin.top})`);
+    
+//     const xExtent = d3.extent(barChartData, d=>d.val);
+//     const xMax = d3.max(barChartData, d=>d.val);
+//     const xScale_v3 = d3.scaleLinear([0, xMax * 1.1], [0, chart_width]);
+    
+//     const yScale = d3.scaleBand().domain(barChartData.map(d=>d.danceability))
+//     .rangeRound([1, chart_height])
+//     .paddingInner(0.05);
+    
+//     const bars = this_svg.selectAll('.bar')
+//     .data(barChartData)
+//     .enter()
+//     .append('rect')
+//     .attr('class','bar')
+//     .attr('x',0)
+//     .attr('y',d => yScale(d.danceability))
+//     .attr('width',d => xScale_v3(d.val))
+//     .attr('height',yScale.bandwidth())
+//     .style('fill', color);
+
+//     const header = this_svg.append('g').attr('class','bar-header')
+//                 .attr('transform',`translate(0,${-chart_margin.top/ 2})`)
+//                 .append('text');
+//     header.append('tspan').text(`${title}`);
+
+
+//     const xAxis = d3.axisTop(xScale_v3)
+//                     .tickSizeInner(-chart_height)
+//                     .tickSizeOuter(0); // the boarder line (boarderless)
+//     const xAxisDraw = this_svg.append('g')
+//                             .attr('class','x axis')
+//                             .call(xAxis);
+//     const yAxis = d3.axisLeft(yScale).tickSize(0);
+//     const yAxisDraw = this_svg.append('g')
+//                             .attr('class','y axis')
+//                             .call(yAxis);
+//     yAxisDraw.selectAll('text').attr('dx','-0.6em');
+
+//     this_svg.append("text")
+//         .attr("x", chart_width / 2)
+//         .attr("y", -chart_margin.top / 2 + 25)
+//         .style("text-anchor", "middle")
+//         .text(`${xLabel}`);
+
+//     this_svg.append("text")
+//         .attr("transform", "rotate(-90)")
+//         .attr("y", 0 - chart_margin.left + 30)
+//         .attr("x",0 - (chart_height / 2))
+//         .attr("dy", "1em")
+//         .style("text-anchor", "middle")
+//         .text("Danceability (* 0.1)");
+// }
+
